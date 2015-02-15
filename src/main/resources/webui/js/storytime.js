2b4e506e944f576c25f08e463d2959a428bab449
AJE Games <ajegames@gmail.com>
2015-02-14 16:43:46 -0800

    Starting to wire the UI with Angular.

--- src/main/resources/webui/js/storytime.js
+++ src/main/resources/webui/js/storytime.js
@@ -1,3 +1,18 @@
 /**
  * Created by dmount on 2/13/15.
  */
+
+(function() {
+  var app = angular.module('storytime', []);
+
+  app.controller('StoryController', function() {
+    this.draft = story;
+  });
+
+  var story = {
+    title: "The Itsy Bitsy Spider",
+    author: "Kids Place Live",
+    tagline: "Watch what this spider does."
+  }
+})();
+
