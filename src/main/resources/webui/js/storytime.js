270ff3392af76f0b309abe940e38a5aaf9b9f051
AJE Games <ajegames@gmail.com>
2015-02-15 21:47:14 -0800

    Hooked up some form fields with static display values for story and scene.

--- src/main/resources/webui/js/storytime.js
+++ src/main/resources/webui/js/storytime.js
@@ -7,12 +7,18 @@
 
   app.controller('StoryController', function() {
     this.draft = story;
+    this.draft.firstScene = scene;
   });
 
   var story = {
     title: "The Itsy Bitsy Spider",
     author: "Kids Place Live",
     tagline: "Watch what this spider does."
+  }
+
+  var scene = {
+    heading: "Introduction",
+    prose: "Once upon a time, there was a tiny, little spider who was thinking about where to go."
   }
 })();
 
