df9735a4f5741f7b5b2a74b905d2e72052eebec9
AJE Games <ajegames@gmail.com>
2015-03-29 09:37:07 -0700

    Separating catalog from story editing

--- src/main/resources/webui/js/storyService.js
+++ src/main/resources/webui/js/storyService.js
@@ -6,54 +6,36 @@
       .service('StoryService', StoryService);
 
   function StoryService($http, $q) {
-
     return ({
-      refreshStories: refreshStories
+      fetchAllStories: fetchAllStories
     });
 
-    function refreshStories() {
+    function fetchAllStories() {
       var request = $http({
         method: "get",
         url: "api/storytime/stories"
       });
-
       return (request.then(handleSuccess, handleError));
     }
 
-    // ---
-    // PRIVATE
-    // ---
+    /// "PRIVATE" FUNCTIONS
 
-    // I transform the error response, unwrapping the application dta from
-    // the API response payload.
+    // unwrap application data
     function handleError(response) {
 
-      // The API response from the server should be returned in a
-      // nomralized format. However, if the request was not handled by the
-      // server (or what not handles properly - ex. server error), then we
-      // may have to normalize it on our end, as best we can.
-      if (
-          !angular.isObject(response.data) || !response.data.message
-      ) {
-
+      // handle unexpected format
+      if (!angular.isObject(response.data) || !response.data.message) {
         return ( $q.reject("An unknown error occurred.") );
-
       }
 
       // Otherwise, use expected error message.
       return ( $q.reject(response.data.message) );
-
     }
 
-
-    // I transform the successful response, unwrapping the application data
-    // from the API response payload.
+    // unwrap application data
     function handleSuccess(response) {
-
       return ( response.data );
-
     }
-
   }
 
 })();
