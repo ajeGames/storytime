36644cdd1157209a6d2ddbff08bed77a210beae6
Dave Mount <davemount@gmail.com>
2015-07-03 10:50:35 -0700

    Trying to get method call to work in angular.  Ugh.

--- src/main/resources/webui/ng/js/story.controller.js
+++ src/main/resources/webui/ng/js/story.controller.js
@@ -8,6 +8,7 @@
         StoryController.$inject = ['$routeParams', 'connectService', 'storyCache'];
 
         function StoryController($routeParams, connectService, storyCache) {
+            console.log('StoryController: called constructor');
             var vm = this;
             //vm.storyCache = storyCache;
             vm.requestedStoryKey = $routeParams.storyKey;
@@ -19,7 +20,14 @@
                 getStory(vm.requestedStoryKey);
             }
 
+            vm.isTheEnd = function() {
+                console.log('StoryController: called isTheEnd');
+                return vm.currentChapter.nextChapterOptions === undefined
+                        || vm.currentChapter.nextChapterOptions.length === 0;
+            }
+
             function getStory(key) {
+                console.log('StoryController: called getStory');
                 connectService.fetchStory(key).then(
                     function (story) {
                         applyRemoteData(story);
@@ -27,12 +35,14 @@
             }
 
             function applyRemoteData(story) {
+                console.log('StoryController: called applyRemoteData');
                 storyCache.activeStory = story;
                 indexChapters(story.firstChapter);
                 getCurrentFromCache();
             }
 
             function indexChapters(chapter) {
+                console.log('StoryController: called indexChapters');
                 if (chapter != null && chapter.id != null) {
                     storyCache.activeChapters[chapter.id] = chapter;
                     for (var i=0, tot=chapter.nextChapterOptions.length; i < tot; i++) {
@@ -42,6 +52,7 @@
             }
 
             function getCurrentFromCache() {
+                console.log('StoryController: called getCurrentFromCache');
                 vm.currentStory = storyCache.activeStory;
                 if (vm.requestedChapterId) {
                     vm.currentChapter = storyCache.activeChapters[vm.requestedChapterId];
