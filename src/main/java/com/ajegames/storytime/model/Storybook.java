ee65119fca26fd46da8724cc2903cd6174f18414
AJE Games <ajegames@gmail.com>
2015-08-29 14:38:27 -0700

    Start to think about how to make the representation instances immutable.

---
+++ src/main/java/com/ajegames/storytime/model/Storybook.java
@@ -0,0 +1,11 @@
+package com.ajegames.storytime.model;
+
+/**
+ * Mutable class that handles dynamic parts of the story, aggregations, indexes, etc.  This is not meant to be passed
+ * around via JSON, but it holds all of the parts that are.  The goal is to allow representation classes to be
+ * immutable, while all of the action is handled here.
+ */
+public class Storybook {
+
+    // TODO think about it
+}
