a61b00e61cd6778774d2167a122cb41741738116
AJE Games <ajegames@gmail.com>
2015-12-12 20:12:09 +0000

    Got this rendering reasonably.

--- react-client/app/components/DecisionPoint.js
+++ react-client/app/components/DecisionPoint.js
@@ -1,31 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 class Sign extends React.Component {
+
+//<li>{this.props.option.teaser}</li>
+
   render() {
+    let uri = "/reader/" + this.props.storyKey + "/" + this.props.option.targetChapterId;
     return (
-        <div key='nextChapter{this.props.sign.targetChapterId}' className="nextChapterOption"><a href="#STORY_KEY/{this.props.sign.targetChapterId}">{this.props.sign.teaser}</a></div>
+        <li><a href={uri}>{this.props.option.teaser}</a></li>
     );
-  };
+  }
 }
 
 class DecisionPoint extends React.Component {
   render() {
-    let choices = [];
+
+    let storyKey = this.props.storyKey;
 
     // TODO different options if end of story
-
-    //this.props.nextOptions.forEach(function(sign) {
-    //  choices.push(<Sign option={sign} />)
-    //});
+    //<ul>
+    //  <li><a href="#/reader/{this.props.storyKey}">Return to Chapter One</a></li>
+    //  <li><a href="#/catalog">Choose Another Story</a></li>
+    //</ul>
 
     return (
-        <div id="decisionPoint">
-          <ul>
-            <li><a href="#/reader/{this.props.storyKey}">Return to Chapter One</a></li>
-            <li><a href="#/catalog">Choose Another Story</a></li>
-          </ul>
-          {choices}
-        </div>
+      <ul id="decisionPoint">
+          {this.props.nextOptions.map(function(option) {
+            return <Sign key={option.targetChapterId} storyKey={storyKey} option={option} />;
+          })}
+      </ul>
     );
   }
 }
