197a1790b0756d14d579ce18ebb39242ed346219
Dave Mount <davemount@gmail.com>
2015-12-18 10:51:09 -0800

    Index chapters and display first chapter by default.

--- react-client/app/components/Reader.js
+++ react-client/app/components/Reader.js
@@ -7,11 +7,20 @@ import STORY1 from './SampleData';  // local test data
 class Reader extends React.Component {
   constructor() {
     super();
+
+    let chapterIndex = new Map();
+    STORY1.chapters.forEach(chapter => {
+      chapterIndex.set(chapter.id, chapter);
+    });
+
     this.state = {
       summary: STORY1.summary,
-      chapters: STORY1.chapters
+      chapters: chapterIndex,
+      currentChapterId: STORY1.summary.firstChapter.targetChapterId
     };
   }
+
+  // TODO respond to change in chapter by updating currentChapterId
 
   render() {
     let storyKeyParam = this.props.params.storyKey;
@@ -35,7 +44,7 @@ class Reader extends React.Component {
       <div id="reader">
         <TitleBar summary={this.state.summary} />
         <p>(story key: {storyKeyParam}, chapter ID: {chapterIdParam})</p>
-        <Chapter chapter={this.state.chapters[0]} storyKey={this.state.summary.key} />
+        <Chapter chapter={this.state.chapters.get(this.state.currentChapterId)} storyKey={this.state.summary.key} />
       </div>
     );
   }
