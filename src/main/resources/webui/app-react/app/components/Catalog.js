42e106c74b669cbd10aecb50fd8e69e5b8b3c45a
AJE Games <ajegames@gmail.com>
2016-01-09 14:44:01 -0800

    Pull in changes from divergent branch by hand.

--- src/main/resources/webui/app-react/app/components/Catalog.js
+++ src/main/resources/webui/app-react/app/components/Catalog.js
@@ -1,71 +1,37 @@
 import React from 'react';
-import $ from 'jquery';
+import { Link } from 'react-router';
+import BackendAccess from './remote/BackendAccess';
 
 class Catalog extends React.Component {
 
   constructor() {
     super();
-
     this.state = { summaries: [] }
   }
 
-  loadStoriesFromServer() {
-    let apiEndpoint = 'http://localhost/storytime/api/storytime/stories';
-    $.ajax({
-      url: apiEndpoint,
-      dataType: 'json',
-      cache: false,
-      success: function(data) {
-        this.setState( { summaries: data } );
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    });
+  componentDidMount() {
+    let backend = new BackendAccess();
+    backend.loadSummaries(this);
   }
 
-  componentDidMount() {
-    this.loadStoriesFromServer();
+  _handleSuccess(summaries) {
+    this.setState( { summaries: summaries });
   }
 
   render() {
     return (
-      <div id="catalog" className="section">
+        <div id="catalog" className="section">
         <h1>Catalog</h1>
-        <h2>How our fair readers select an adventure</h2>
-        <form>
-          <p>
-              <label>Keywords: <input id="search_keyword" placeholder="Enter a keyword or phrase" type="text"/></label>
-              <button>Search</button>
-          </p>
-        </form>
-        <div>Results:
-          <ul>
-            {this.state.summaries.map(function(summary) {
-              return <li key={summary.key}><Link to={'#/reader/{summary.key}'}>{summary.title}</Link> by {summary.author}<br/>{summary.tagLine}</li>
-            })}
-          </ul>
-          <p>Sample:</p>
-          <ul>
-            <li><a href="#/reader/o9s0toym">The Cave</a>, by Bubba Gump.<br/>See what you'll find when you stuble upon a cave in the woods.</li>
-          </ul>
-        </div>
-        <div>
-          <h2>Notes:</h2>
-          <ul>
-            <li>Search: title, author, genre, recency of publication</li>
-            <li>Genre
-              <ul>
-                <li>Rank by most popular, publish date</li>
-                <li>List with paging</li>
-              </ul>
-            </li>
-            <li>Target ages / "movie" ratings</li>
-            <li>Reader voting -- 'like' a story</li>
-          </ul>
-        </div>
-      </div>
-    );
+        <h2>Choose an adventure:</h2>
+    <div>
+    <ul>
+    {this.state.summaries.map(function(summary) {
+      return <li key={summary.key}><Link to={'/reader/' + summary.key}>{summary.title}</Link> by {summary.author}<br/>{summary.tagLine}</li>
+    })}
+  </ul>
+    </div>
+    </div>
+  );
   }
 }
 
