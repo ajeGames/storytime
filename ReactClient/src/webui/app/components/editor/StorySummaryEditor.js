99a811308b8d3ca4578b7d63445c7ec94ba20f55
AJE Games <ajegames@gmail.com>
2016-02-21 09:20:20 -0800

    building story summary editor

--- ReactClient/src/webui/app/components/editor/StorySummaryEditor.js
+++ ReactClient/src/webui/app/components/editor/StorySummaryEditor.js
@@ -7,31 +7,47 @@ class StorySummaryEditor extends React.Component {
     super();
   }
 
+  handleChange(event) {
+  }
+
   render() {
     return (
         <div>
           <h2>StorySummaryEditor</h2>
-          <div>
-            <label>Title:
-              <input type="text" />
-            </label>
+          <div className="section">
+            <div>Key: jk3jko8w</div>
+            <div>Title: The Cave</div>
+            <div>Author: Bubba Gump</div>
+            <div>Tag Line: Step inside, find adventure!</div>
+            <div>Description: You could sit on your couch all day, tapping away at your smart phone, or you could get
+              outside and have an adventure.
+            </div>
           </div>
-          <div>
-            <label>Author:
-              <input type="text" />
-            </label>
+          <div className="section">
+            <div>
+              <label>Title:
+                <input type="text" value="The Cave" onChange={this.handleChange} />
+              </label>
+            </div>
+            <div>
+              <label>Author:
+                <input type="text" value="Bubba Gump" onChange={this.handleChange} />
+              </label>
+            </div>
+            <div>
+              <label>Tag Line:
+                <input type="text" value="Step inside, find adventure!" onChange={this.handleChange} />
+              </label>
+            </div>
+            <div>
+              <label>Description:
+                <textarea onChange={this.handleChange}
+                          value="You could sit on your couch all day, tapping away at your smart phone, or you could get
+              outside and have an adventure."></textarea>
+              </label>
+            </div>
+            <button>Save Story Summary</button>
           </div>
-          <div>
-            <label>Tag Line:
-              <input type="text" />
-            </label>
-          </div>
-          <div>
-            <label>Description:
-              <textarea></textarea>
-            </label>
-          </div>
-          <button>Save Story Summary</button>
         </div>
     );
   }
