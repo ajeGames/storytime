abff3cd7fa7adb9fee2bd2a929a1910359bae654
AJE Games <ajegames@gmail.com>
2016-02-27 10:13:02 -0800

    start writing reducers with tests

--- ReactClient/src/webui/app/reducers.js
+++ ReactClient/src/webui/app/reducers.js
@@ -1,3 +1,16 @@
-/**
- * Created by dmount on 2/20/16.
- */
+export function setTitle(state, title) {
+  return state.set('title', title);
+}
+
+export function setAuthor(state, author) {
+  return state.set('author', author);
+}
+
+export function setTagLine(state, tagLine) {
+  return state.set('tagLine', tagLine);
+}
+
+export function setAbout(state, about) {
+  return state.set('about', about);
+}
+
