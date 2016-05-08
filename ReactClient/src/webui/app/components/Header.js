2b8806c373b7ab3d493efa4f23fec1b7b32f34a6
Dave Mount <dmount@guidewire.com>
2016-05-08 16:13:08 -0700

    Add rudimentary navigation

--- ReactClient/src/webui/app/components/Header.js
+++ ReactClient/src/webui/app/components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 export default class Header extends React.Component {
   render() {
     return (
         <div id="masthead" className="masthead">
           <h1>{this.props.heading}</h1>
+          <menu>
+            <Link to="/">Home</Link> | <Link to="/library">Library</Link> | <Link to="/story">Story</Link> | <Link to="/editor">Editor</Link>
+          </menu>
         </div>
     );
   }
