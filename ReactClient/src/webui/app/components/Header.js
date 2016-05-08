4f3288835b8fc783da6c5d6c015cf79b1652591f
Dave Mount <dmount@guidewire.com>
2016-05-08 16:22:38 -0700

    Highlight active menu item -- not exactly what I wanted since it highlight home ('/') along with its subpaths.

--- ReactClient/src/webui/app/components/Header.js
+++ ReactClient/src/webui/app/components/Header.js
@@ -7,7 +7,7 @@ export default class Header extends React.Component {
         <div id="masthead" className="masthead">
           <h1>{this.props.heading}</h1>
           <menu>
-            <Link to="/">Home</Link> | <Link to="/library">Library</Link> | <Link to="/story">Story</Link> | <Link to="/editor">Editor</Link>
+            <Link to="/" activeClassName="active-menu-item">Home</Link> | <Link to="/library" activeClassName="active-menu-item">Library</Link> | <Link to="/story" activeClassName="active-menu-item">Story</Link> | <Link to="/editor" activeClassName="active-menu-item">Editor</Link>
           </menu>
         </div>
     );
