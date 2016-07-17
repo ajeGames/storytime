52eb9bf66888fb1989c3bb5c21ef945f4ca12bb9
Dave Mount <davemount@gmail.com>
2016-07-17 11:41:09 -0700

    Attempt more refactoring.  Need to learn how to handle callbacks correctly to enforce sequencing of dynamo functions.  (Delete needs to complete before create, create before list tables, etc.)

--- aws/dynamodb/CommonAdmin.js
+++ aws/dynamodb/CommonAdmin.js
@@ -5,12 +5,19 @@ AWS.config.update({
   endpoint: "http://localhost:8000"
 });
 
-exports.dynamodb = new AWS.DynamoDB();
+var dynamodb = new AWS.DynamoDB();
 
-exports.handleResponse = function(err, data) {
+var handleResponse = function(err, data) {
   if (err) {
     console.error("Unable to perform action. Error:", JSON.stringify(err, null, 2));
   } else {
     console.log("Action completed. Result:", JSON.stringify(data, null, 2));
   }
 };
+
+exports.listTables = function() {
+  dynamodb.listTables({}, handleResponse);
+};
+
+exports.dynamodb = dynamodb;
+exports.handleResponse = handleResponse;
--- aws/index.js
+++ aws/index.js
@@ -1,12 +1,21 @@
 var StoriesTable = require('./dynamodb/StoriesTable');
 var ChaptersTable = require('./dynamodb/ChaptersTable');
 
-var reset = function() {
+var admin = require('./dynamodb/CommonAdmin');
+
+// TODO not using aynch correctly; these steps need to be gated by callbacks
+
+var destroyAllTables = function() {
   // wipe out everything
   StoriesTable.delete();
   ChaptersTable.delete();
+}
 
+var createAllTables = function() {
   // create new tables
   StoriesTable.create();
   ChaptersTable.create();
 }
+
+createAllTables();
+admin.listTables();
