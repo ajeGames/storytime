4c74d4ac8ea786616115434a583ac32fcdff642f
Dave Mount <davemount@gmail.com>
2016-12-28 14:42:38 -0800

    Coded and tested manually: status, getStories, createStory, getStory.

--- service/serverless/stories/handler.js
+++ service/serverless/stories/handler.js
@@ -10,52 +10,79 @@ const headers = {
   'Access-Control-Allow-Origin' : '*', // Required for CORS support to work
 };
 
-const sampleStory = {
-  key: '1111-imauniqukey',
-  title: 'Walk About the Neighborhood',
-  author: 'Evangeline Sora',
-  tagLine: 'Ooo, look at all the pretty lights.',
-  firstChapter: {
-    chapterId: 1,
-    teaser: 'Your destiny starts here.'
-  }
-};
-
 function prettyJsonLog(thing, description) {
   console.log(description, JSON.stringify(thing, null, 2));
 }
 
-module.exports.getStatus = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
+function buildSuccess(payload, statusCode) {
+  let code = statusCode ? statusCode : 200;
+  return {
+    statusCode: code,
+    headers: headers,
+    body: JSON.stringify(payload),
+  };
+}
+
+function buildError(statusCode, errorCode, errorMessage) {
+  return {
+    statusCode: statusCode,
     headers: headers,
     body: JSON.stringify({
-      salutation: 'The StoryTime service is alive and well. Thanks for asking.',
-      status: 'All systems are go.',
-      version: version,
+      errorCode: errorCode,
+      message: errorMessage,
     }),
   };
-  callback(null, response);
+}
+
+function buildErrorRequiredField(fieldName) {
+  return buildError('400', 'MissingRequiredField',
+    'Request body missing required field: ' + fieldName);
+}
+
+function buildErrorNotFound(key) {
+  return buildError('404', 'NotFound', 'Nothing found with key: ' + key);
+}
+
+function buildErrorDataAccess(err) {
+  prettyJsonLog(err, 'dynamodb err');
+  return buildError('500', 'DataAccess', err.message);
+}
+
+/**
+ * Checks the status of the service; returns signs of life if possible.
+ */
+module.exports.getStatus = (event, context, callback) => {
+  const payload = {
+    salutation: 'The StoryTime service is alive and well. Thanks for asking.',
+    status: 'All systems are go.',
+    version: version,
+  };
+  callback(null, buildSuccess(payload));
 };
 
+/**
+ * Returns summaries of all stories in the system.  Someday this will have to
+ * be limited with filters and pagination.
+ */
 module.exports.getSummaries = (event, context, callback) => {
   const params = {
     TableName: storyTableName,
     AttributesToGet: [ 'summary' ],
     ConsistentRead: true,
   };
-  const processResults = (err, res) => callback(null, {
-    statusCode: err ? '500' : '200',
-    headers: headers,
-    body: err ? err.message : JSON.stringify(res.Items),
-  });
+  const processResults = (err, res) => {
+    if (err) {
+      callback(null, buildErrorDataAccess(err));
+    } else {
+      callback(null, buildSuccess(res.Items));
+    }
+  };
   dynamodbClient.scan(params, processResults);
 }
 
-function retrieveStory(storyKey) {
-
-}
-
+/**
+ * Returns the story with the given storyKey or 404 if not found.
+ */
 module.exports.getStory = (event, context, callback) => {
   const storyKey = event.pathParameters.storyKey;
   const params = {
@@ -65,37 +92,46 @@ module.exports.getStory = (event, context, callback) => {
     },
   };
   const processResults = (err, res) => {
-    prettyJsonLog(err ? err : res, 'getStory');
-    callback(null, {
-      statusCode: err ? '404' : '200',
-      headers: headers,
-      body: err ? err.message : JSON.stringify(res.Item.summary),
-    });
+    if (err) {
+      callback(null, buildErrorDataAccess(err));
+    } else {
+      if (res.Item === undefined) {
+        callback(null, buildErrorNotFound(storyKey));
+      } else {
+        callback(null, buildSuccess(res.Item.summary));
+      }
+    }
   };
   dynamodbClient.get(params, processResults);
 }
 
-function generateStoryKey() {
-  const length = 12;
+function generateStoryKey(length) {
+  const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   let key = "";
-  let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   for(var i = 0; i < length; i++) {
     key += possible.charAt(Math.floor(Math.random() * possible.length));
   }
   return key;
 }
 
+/**
+ * Adds a new story with a unique storyKey to database.
+ *
+ * (There is a minute chance of failure if a key is generated that is
+ * already in use.  Just try a second time -- the odds are nearly certain
+ * that it won't happen twice in a row unless there's a bug in the service.)
+ */
 module.exports.createStory = (event, context, callback) => {
   const body = JSON.parse(event.body);
 
   // validate input
   if (body.title === undefined) {
-    callback(new Error('[400] Validation Error: missing required field: title'));
+    callback(null, buildErrorRequiredField('title'));
     return;
   }
+  // TODO handle unexpected types (i.e., non-String)
 
-  // insert story summary
-  const storyKey = generateStoryKey();  // TODO make sure key not already in use
+  const storyKey = generateStoryKey(12);  // TODO make sure key not already in use
   const summary = {
     key: storyKey,
     title: body.title,
@@ -116,12 +152,11 @@ module.exports.createStory = (event, context, callback) => {
     ConditionExpression: 'attribute_not_exists(storyKey)',  // do not overwrite existing
   };
   const processResults = (err, res) => {
-    prettyJsonLog(err ? err : res, 'getStory');
-    callback(null, {
-      statusCode: err ? '500' : '202',
-      headers: headers,
-      body: err ? err.message : JSON.stringify(summary),
-    });
-  }
+    if (err) {
+      callback(null, buildErrorDataAccess(err));
+    } else {
+      callback(null, buildSuccess(summary, 201));
+    }
+  };
   dynamodbClient.put(params, processResults);
 }
