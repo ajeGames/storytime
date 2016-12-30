9b0cd8d62456556184ae711ae56e697a5bbbdd44
Dave Mount <davemount@gmail.com>
2016-12-29 16:25:48 -0800

    resetting test infrastructure

--- service/aws/test/persistence_spec.js
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai';
-import { isAlive } from './src/dynamodb/CommonAdmin';
-
-describe('Common Admin', () => {
-  it('Can tell that DynamoDB is responsive', () => {
-    expect(isAlive()).is.true;
-  });
-});
