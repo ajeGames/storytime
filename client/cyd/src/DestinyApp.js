bc0039194f4ab1c1bf97ae00ad6f8e26d6ae4b86
Dave Mount <davemount@gmail.com>
2017-03-04 13:39:34 -0800

    hacking in bootstrap design

---
+++ client/cyd/src/DestinyApp.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Header from './components/Header';
+import Library from './components/Library';
+
+class DestinyApp extends Component {
+  render() {
+    return (
+      <div>
+        <Library />
+      </div>
+    );
+  }
+}
+
+export default DestinyApp;
