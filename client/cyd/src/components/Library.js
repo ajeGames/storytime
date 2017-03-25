a40e51a974a313352f2c59e3595dd7e963538310
Dave Mount <davemount@gmail.com>
2017-03-25 08:13:27 -0700

    working on library

--- client/cyd/src/components/Library.js
+++ client/cyd/src/components/Library.js
@@ -1,6 +1,77 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-class Library extends Component {
+const stories = [
+  {
+    key: "o9s0toym",
+    title: "The Cave",
+    author: "Bubba Gumpzzz",
+    tagLine: "You want to read this.",
+    about: "Sometimes life compels you great things.  And sometimes you'd rather play this game.",
+    firstChapter: 1000
+  },
+  {
+    "key": "5qcao3fu",
+    "title": "Where the Water Goes",
+    "author": "Mr. Bubbles",
+    "tagLine": "Round and round and down and down",
+    "about": "What happens when someone pulls the plug?",
+    "firstChapter": 1000
+  },
+  {
+    key: "12345678",
+    title: "New Story A",
+    author: "Bubba Gumpzzz",
+    tagLine: "You want to read this.",
+    about: "Sometimes life compels you great things.  And sometimes you'd rather play this game.",
+    firstChapter: 1000
+  },
+  {
+    "key": "09876543",
+    "title": "New Story B",
+    "author": "Mr. Bubbles",
+    "tagLine": "Round and round and down and down",
+    "about": "What happens when someone pulls the plug?",
+    "firstChapter": 1000
+  },
+  {
+    "key": "qwertyui",
+    "title": "New Story C",
+    "author": "Mr. Bubbles",
+    "tagLine": "Round and round and down and down",
+    "about": "What happens when someone pulls the plug?",
+    "firstChapter": 1000
+  },
+];
+
+function StoryCard(props) {
+  return (
+    <div className="col-md-4">
+        <div className="catalog-item">
+            <p><span className="title"><Link to="/stories/{props.story.key}/{props.story.firstChapter}">{props.story.title}</Link></span></p>
+            <p><em>by {props.story.author}</em></p>
+            <p>{props.story.tagLine}</p>
+            <p>{props.story.about}</p>
+        </div>
+    </div>
+  );
+}
+
+function CardCatalog(props) {
+  const storyCards = props.stories.map((story) =>
+    <StoryCard key={story.key} story={story} />
+  );
+
+  return (
+    <div className="container">
+      <div className="row">
+        {storyCards}
+      </div>
+    </div>
+  );
+}
+
+class Library extends React.Component {
   render() {
     return (
       <div className="panel panel-default">
@@ -8,54 +79,7 @@ class Library extends Component {
               <h3 className="text-center panel-title">Welcome to the Library. Choose a Story.</h3></div>
           <div className="panel-body">
               <div className="container">
-                  <div className="row">
-                      <div className="col-md-4">
-                          <div className="catalog-item">
-                              <p><span className="title"><a href="#">A Series of Unfortunate Events</a></span></p>
-                              <p><em>by Lemony Snickett</em></p>
-                              <p>This is a very sad story about a very sad series of unfortunate events. You probably don't want to read this one. It will make you sad.</p>
-                          </div>
-                      </div>
-                      <div className="col-md-4">
-                          <div className="catalog-item">
-                              <p><span className="title"><a href="#">The Cave</a></span></p>
-                              <p><em>by Bubbabubba Gump</em></p>
-                              <p>Explore deep and discover the untold mysteries.</p>
-                          </div>
-                      </div>
-                      <div className="col-md-4">
-                          <div className="catalog-item">
-                              <p><span className="title"><a href="#">Along the Waterfall</a></span></p>
-                              <p><em>by Mary Spring</em></p>
-                              <p>Mary, Mary, quite contrary, how does your garden grow? With silver bells and cockle shells and pretty maids all in a row. Hey diddle diddle, the cat and the fiddle, the cow jumped over the moon. The little dog laughed to see
-                                  such sport, and the dish ran away with the spoon.</p>
-                          </div>
-                      </div>
-                  </div>
-                  <div className="row">
-                      <div className="col-md-4">
-                          <div className="catalog-item">
-                              <p><span className="title"><a href="#">The Cave</a></span></p>
-                              <p><em>by Bubbabubba Gump</em></p>
-                              <p>Explore deep and discover the untold mysteries.</p>
-                          </div>
-                      </div>
-                      <div className="col-md-4">
-                          <div className="catalog-item">
-                              <p><span className="title"><a href="#">Along the Waterfall</a></span></p>
-                              <p><em>by Mary Spring</em></p>
-                              <p>Mary, Mary, quite contrary, how does your garden grow? With silver bells and cockle shells and pretty maids all in a row. Hey diddle diddle, the cat and the fiddle, the cow jumped over the moon. The little dog laughed to see
-                                  such sport, and the dish ran away with the spoon.</p>
-                          </div>
-                      </div>
-                      <div className="col-md-4">
-                          <div className="catalog-item">
-                              <p><span className="title"><a href="#">A Series of Unfortunate Events</a></span></p>
-                              <p><em>by Lemony Snickett</em></p>
-                              <p>This is a very sad story about a very sad series of unfortunate events. You probably don't want to read this one. It will make you sad.</p>
-                          </div>
-                      </div>
-                  </div>
+                <CardCatalog stories={stories} />
               </div>
           </div>
       </div>
