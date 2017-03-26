80ca3499e80f6f2ed6db31ec7c37d4e29fd6cb7b
Dave Mount <davemount@gmail.com>
2017-03-25 17:44:24 -0700

    save

--- client/cyd/src/api/sample-stories.js
+++ client/cyd/src/api/sample-stories.js
@@ -54,15 +54,6 @@ export const story = {
     id: 1000,
     title: "The Cave",
     prose: "It's a nice day, so you decide to go for a walk.  As you stroll along, you see a clearing with an outcrop of rocks.  Something is unusual about these rocks, some blackness unlike the surrounding soil.  You notice that this is an opening to a cave.  It is dark inside, and you wonder how far it goes.\n\nWhat would you like to do?",
-    signPost: [
-      {
-        chapterId: 1001,
-        teaser: "Step inside."
-      },
-      {
-        chapterId: 1002,
-        teaser: "Continue walking."
-      }
-    ]
+    signPost: []
   }
 };
--- client/cyd/src/components/Reader.js
+++ client/cyd/src/components/Reader.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { story } from '../api/sample-stories';
 
-const Sign = (props) => (
-  <li>
-    <Link to={`/reader/${props.storyKey}/${props.destination.chapterId}`}>
-      {props.destination.teaser}
-    </Link>
-  </li>
-);
+const Sign = (props) => {
+  return (
+    <li>
+      <Link to={`/reader/${props.storyKey}/${props.destination.chapterId}`}>
+        {props.destination.teaser}
+      </Link>
+    </li>
+  );
+}
 
 Sign.propTypes = {
   storyKey: React.PropTypes.string,
@@ -18,25 +20,25 @@ Sign.propTypes = {
   }),
 };
 
-const EndOfLine = (props) => {
+const TheEnd = (props) => {
   return (
     <div>
       <p>You are at the end of the line.</p>
       <ul>
-        <li><Link to="/story/{props.storyKey}">Return to Chapter One</Link></li>
+        <li><Link to={`/story/${props.storyKey}`}>Return to Chapter One</Link></li>
         <li><Link to="/">Choose Another Story</Link></li>
       </ul>
     </div>
   );
 }
 
-EndOfLine.propTypes = {
+TheEnd.propTypes = {
   storyKey: React.PropTypes.string,
 };
 
 const SignPost = (props) => {
   if (props.signs.length === 0) {
-    return <EndOfLine />
+    return <TheEnd storyKey={props.storyKey} />
   }
   return (
     <ul>
@@ -64,15 +66,27 @@ const Chapter = (props) => (
 )
 
 class Reader extends Component {
+  constructor() {
+    super();
+    this.state = {
+      story: story.summary,
+      chapter: story.chapter
+    }
+  }
+
+  handleChangeChapter(chapter) {
+    this.setState({ chapter });
+  }
+
   render() {
     return (
       <div className="panel panel-default">
         <div className="panel-heading">
           <h3 className="text-center panel-title">
-            <b>{story.summary.title}</b> <em>by {story.summary.author}</em></h3>
+            <b>{this.state.story.title}</b> <em>by {this.state.story.author}</em></h3>
         </div>
         <div className="panel-body">
-          <Chapter storyKey={story.summary.key} chapter={story.chapter} />
+          <Chapter storyKey={this.state.story.key} chapter={this.state.chapter} />
         </div>
       </div>
     );
