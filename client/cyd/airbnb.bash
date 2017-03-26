a860bc7fdf3a8b197d56019c9bde9488a7ecd963
Dave Mount <davemount@gmail.com>
2017-03-26 15:02:07 -0700

    ejected from create-react-app; struggling to get linting working in atom

---
+++ client/cyd/airbnb.bash
@@ -0,0 +1,5 @@
+(
+export PKG=eslint-config-airbnb-base;
+npm info "$PKG@latest" peerDependencies --json | command sed 's/[\{\},]//g ; s/: /@/g' | xargs npm install --save-dev "$PKG@latest"
+)
+
--- client/cyd/src/App.js
+++ client/cyd/src/App.js
@@ -1,44 +1,62 @@
-import React, {Component} from 'react'
+import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
-  Link
-} from 'react-router-dom'
+  Link,
+} from 'react-router-dom';
 
 import Library from './components/Library';
 import Reader from './components/Reader';
 import Clubhouse from './components/Clubhouse';
 import WritingDesk from './components/WritingDesk';
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <nav className="navbar navbar-default navigation-clean-button">
-            <div className="container-fluid">
-              <div className="navbar-header"><Link className="navbar-brand navbar-link" to="/">StoryTime </Link>
-                <button className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span className="sr-only">Toggle navigation</span><span className="icon-bar"></span><span className="icon-bar"></span><span className="icon-bar"></span></button>
-              </div>
-              <div className="collapse navbar-collapse" id="navcol-1">
-                <ul className="nav navbar-nav">
-                  <li role="presentation"><Link to="/">Library</Link></li>
-                  <li role="presentation"><Link to="/desk">Writing Desk</Link></li>
-                  <li role="presentation"><Link to="/clubhouse">Clubhouse</Link></li>
-                </ul>
-                <p className="navbar-text navbar-right actions"><a className="navbar-link login" href="#">Log In</a> <a className="btn btn-default action-button" role="button" href="#">Sign Up</a></p>
-              </div>
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav className="navbar navbar-default navigation-clean-button">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <Link
+                className="navbar-brand navbar-link"
+                to="/"
+              >
+                StoryTime
+              </Link>
+              <button
+                className="navbar-toggle collapsed"
+                data-toggle="collapse"
+                data-target="#navcol-1"
+              >
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar" />
+                <span className="icon-bar" />
+                <span className="icon-bar" />
+              </button>
             </div>
-          </nav>
+            <div className="collapse navbar-collapse" id="navcol-1">
+              <ul className="nav navbar-nav">
+                <li role="presentation"><Link to="/">Library</Link></li>
+                <li role="presentation"><Link to="/desk">Writing Desk</Link></li>
+                <li role="presentation"><Link to="/clubhouse">Clubhouse</Link></li>
+              </ul>
+              <p className="navbar-text navbar-right actions">
+                <a className="navbar-link login" href="/login">Log In</a>
+                <a
+                  className="btn btn-default action-button"
+                  role="button" href="/signup"
+                >Sign Up</a></p>
+            </div>
+          </div>
+        </nav>
 
-          <Route exact path="/" component={Library} />
-          <Route path="/reader/:storyKey/:chapterId" component={Reader} />
-          <Route path="/desk" component={WritingDesk} />
-          <Route path="/clubhouse" component={Clubhouse} />
-        </div>
-      </Router>
-    );
-  }
+        <Route exact path="/" component={Library} />
+        <Route path="/reader/:storyKey/:chapterId" component={Reader} />
+        <Route path="/desk" component={WritingDesk} />
+        <Route path="/clubhouse" component={Clubhouse} />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
