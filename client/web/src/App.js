06ac9568e535ce67407a6e4c2e36a5884efd5586
Dave Mount <davemount@gmail.com>
2017-04-09 18:09:44 -0700

    added Reader

--- client/web/src/App.js
+++ client/web/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Library from './components/Library';
+import Reader from './components/Reader';
 import NumberGuessing from './pages/NumberGuessing';
 
 const App = () => (
@@ -9,6 +10,7 @@ const App = () => (
         <h3>StoryTime</h3>
         <p><Link to="/">StoryTime</Link> | <Link to="/guess">Number Guessing Game</Link></p>
         <Route exact path="/" component={Library} />
+        <Route path="/reader" component={Reader} />
         <Route path="/guess" component={NumberGuessing} />
       </div>
     </Router>
