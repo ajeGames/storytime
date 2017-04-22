93b4198c580a64865fbdda20c20d9906bff49866
Dave Mount <davemount@gmail.com>
2017-04-22 12:56:42 -0700

    redux wip

--- client/web/src/appstate/reducers/__tests__/bookshelf.test.js
+++ client/web/src/appstate/reducers/__tests__/bookshelf.test.js
@@ -1,13 +1,50 @@
-import bookshelf from '../bookshelf';
+import { bookshelf } from '../bookshelf';
 import { loadStorySummary } from '../../actions';
 
-it('loads story correctly', () => {
-  const testSummary = {
-    storyKey: '123',
-    title: 'hello world',
-    author: 'bubba gump'
-  }
+const testSummary = {
+  storyKey: 'story123',
+  title: 'hello world',
+  author: {
+    id: 'author::1234',
+    penName: 'Bubba Gump'
+  },
+  tagLine: 'every little thing is gonna be alright',
+  about: 'this should draw you in',
+  firstChapter: 'chapter123'
+}
+
+// const testChapter = {
+//   chapterId: 'chapter123',
+//   title: 'Danger Lies Within',
+//   prose: 'blah blah blah blah blah blah with some kind of handlebars for styling, image placement, etc.',
+//   signpost: [
+//     {
+//       destination: 'uniquechapterkey3',
+//       teaser: 'Take this path, sucker.'
+//     },
+//     {
+//       destination: 'uniquechapterkey4',
+//       teaser: 'You are making the right choice, dupe.'
+//     }
+//   ]
+// }
+
+it('loads single story correctly', () => {
   const after = bookshelf(undefined, loadStorySummary(testSummary));
-  console.log(after);
-  expect(after['123'].summary.title).toEqual(testSummary.title);
+
+  expect(after['story123']).toBeDefined();
+  expect(after['story123'].summary).toEqual(testSummary);
 });
+
+it('loads multiple stories correctly', () => {
+  const first = bookshelf(undefined, loadStorySummary(testSummary));
+  const anotherStory = Object.assign({}, testSummary, { storyKey: 'blargy' });
+  let next = bookshelf(first, loadStorySummary(anotherStory));
+
+  expect(next[testSummary.storyKey]).toBeDefined();
+  expect(next[anotherStory.storyKey]).toBeDefined();
+})
+//
+// it('load a chapter correctly', () => {
+//   const after = bookshelf(undefined, loadChapter('story456', testChapter));
+// });
--- client/web/src/appstate/reducers/bookshelf.js
+++ client/web/src/appstate/reducers/bookshelf.js
@@ -1,48 +1,73 @@
-/*
-  bookshelf: {
-    astorykey: {
-      ...summary,
-      chapters: {
-        achapterid: {},
-        bchapterid: {},
-        cchapterid: {},
-      }
-    },
-    bstorykey: {
-      ...summary,
-      chapters: {
-        achapterid: {},
-        bchapterid: {},
-      }
-    }
-  }
-}
-*/
-
 import { LOAD_STORY_SUMMARY, LOAD_CHAPTER } from '../actions';
-import { shortStory } from '../../apidata';
 
-let initialState = {};
-initialState[shortStory.summary.storyKey] = {
-  summary: shortStory.summary,
-  chapters: shortStory.chapters
-};
-
-function prepSummaryForStorage(summary) {
-  let node = {};
-  node[summary.storyKey] = {
-    summary
-  };
-  return node;
-}
-
-const bookshelf = (state = initialState, action) => {
+const summary = (state = {}, action) => {
   switch (action.type) {
     case LOAD_STORY_SUMMARY:
-      console.log('From bookshelf reducer: ' + JSON.stringify(action));
-      return Object.assign({}, state,
-         prepSummaryForStorage(action.payload));
+      const story = action.payload;
+      return {
+        storyKey: story.storyKey,
+        title: story.title,
+        author: {
+          id: story.author.id,
+          penName: story.author.penName
+        },
+        tagLine: story.tagLine,
+        about: story.about,
+        firstChapter: story.firstChapter
+      };
+    default:
+      return state;
+  }
+}
+
+const chapter = (state = {}, action) => {
+  switch (action.type) {
     case LOAD_CHAPTER:
+      const chapter = action.payload;
+      const mySignpost = (chapter.signpost)
+        ? chapter.signpost.map(sign => {
+          return {
+            destination: sign.destination,
+            teaser: sign.teaser
+          }
+        })
+        : [];
+      return {
+        id: chapter.id,
+        title: chapter.title,
+        prose: chapter.prose,
+        signpost: mySignpost
+      };
+    default:
+      return state;
+  }
+}
+
+export const story = {
+  summary,
+  chapters: {
+    // ...chaptersById
+  }
+};
+
+export const bookshelf = (state = {}, action) => {
+  switch (action.type) {
+    case LOAD_STORY_SUMMARY:
+      const storySummary = summary(undefined, action);
+      let node = {};
+      node[storySummary.storyKey] = {
+        summary: storySummary
+      }
+      return Object.assign({}, state, node);
+    case LOAD_CHAPTER:
+      // const storyKey = action.payload.storyKey;
+      // const chapter = action.payload.chapter;
+      // return Object.assign({}, state, {
+      //   state[storyKey]: {
+      //     chapters: prepChapterForStorage(chapter)
+      //   }
+      // });
+      return state;
     default:
       return state;
   }
